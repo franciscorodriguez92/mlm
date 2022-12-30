a03706e713c3e4dfa8a51d90bdd105594890abbd
Francisco Miguel Rodriguez Sanchez <fran@Franciscos-MacBook-Pro.local>
2022-12-30 11:58:45 +0100

    Add architecture with sentence bert

--- src/utils/architectures.py
+++ src/utils/architectures.py
@@ -62,3 +62,62 @@ class Transformer_lstm_model(torch.nn.Module):
               return (loss,)
           else:
               return (linear_output,)
+
+class transformer_sbert_lstm_model(torch.nn.Module):
+    def __init__(self, model_mlm, model_mlm2, num_labels=2, focal_loss=False):
+          super(transformer_sbert_lstm_model, self).__init__()
+          self.focal_loss = focal_loss
+          self.num_labels = num_labels
+          self.model_mlm = model_mlm
+          self.model_mlm2 = model_mlm2
+          self.encoder = AutoModel.from_pretrained(self.model_mlm)
+          self.encoder2 = AutoModel.from_pretrained(self.model_mlm2)
+          self.lstm = torch.nn.LSTM(768, 256, batch_first=True,bidirectional=True)
+          self.dropout = torch.nn.Dropout(0.1)
+          #self.linear = torch.nn.Linear(256*2, self.num_labels)
+          self.linear = torch.nn.Linear((256*2) + 768, (256*2))
+          self.linear2 = torch.nn.Linear((256*2), self.num_labels)
+            
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+            input_mask_expanded.sum(1), min=1e-9)
+          
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
+          outputs = self.encoder(
+               input_ids, 
+               attention_mask=attention_mask,
+               token_type_ids= token_type_ids,
+               output_attentions=False,
+               output_hidden_states=False)
+          sequence_output = outputs ['last_hidden_state']
+          outputs_encoder2 = self.encoder2(
+               input_ids, 
+               attention_mask=attention_mask)
+          sentence_embeddings = self.mean_pooling(outputs_encoder2, attention_mask)
+          #print("sequence_output", outputs[1].shape)
+          # sequence_output has the following shape: (batch_size, sequence_length, 768)
+          lstm_output, (h,c) = self.lstm(sequence_output) # extract the 1st token's embeddings
+          hidden = torch.cat((lstm_output[:,-1, :256],lstm_output[:,0, 256:]),dim=-1)
+          hidden = self.dropout(hidden)
+          #linear_output = self.linear(hidden.view(-1,256*2))
+          linear_output = self.linear(torch.cat((hidden.view(-1,256*2), sentence_embeddings), 1))
+          linear_output = self.linear2(linear_output) 
+          loss = None
+          if labels is not None:
+              if self.focal_loss:
+                  loss_fct = FocalLoss(ignore_index=-1)
+              else:
+                  loss_fct = CrossEntropyLoss(ignore_index=-1)
+              #print("labels::", self.num_labels)
+              #print("labels::", labels)
+
+              loss = loss_fct(linear_output.view(-1, self.num_labels), labels.view(-1))
+              #loss2 = loss_fct(logits_task2.view(-1, self.num_labels_task2), labels[:,1].view(-1))
+              #loss = loss1 + loss2
+              #rint("LOSS::::::::::::::", loss.item())
+              #return (loss,)
+              return (loss,)
+          else:
+              return (linear_output,)
